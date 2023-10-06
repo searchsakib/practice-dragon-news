616d6d4fc3a0707a718ace06e31e0981f85751ef
searchsakib <searchsakib@gmail.com>
2023-10-06 13:46:52 +0600

    login & register initially done

--- src/pages/Shared/Login/Login.jsx
+++ src/pages/Shared/Login/Login.jsx
@@ -1,7 +1,50 @@
+import { Link } from 'react-router-dom';
+import Navbar from '../Navbar/Navbar';
+
 const Login = () => {
   return (
     <div>
-      <h2>This is Login</h2>
+      <Navbar></Navbar>
+      <div className="mx-auto w-1/2 ">
+        <form className="card-body">
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input
+              type="email"
+              placeholder="email"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Password</span>
+            </label>
+            <input
+              type="password"
+              placeholder="password"
+              className="input input-bordered"
+              required
+            />
+            <label className="label">
+              <a href="#" className="label-text-alt link link-hover">
+                Forgot password?
+              </a>
+            </label>
+          </div>
+          <div className="form-control mt-6">
+            <button className="btn btn-primary">Login</button>
+          </div>
+        </form>
+        <p className="font-medium text-center">
+          Do not have an account{' '}
+          <Link to="/register" className="font-semibold text-blue-500">
+            Register
+          </Link>{' '}
+        </p>
+      </div>
     </div>
   );
 };
--- src/pages/Shared/Register/Register.jsx
+++ src/pages/Shared/Register/Register.jsx
@@ -1,7 +1,63 @@
+import Navbar from '../Navbar/Navbar';
+
 const Register = () => {
   return (
     <div>
-      <h2>This is Register Page</h2>
+      <Navbar></Navbar>
+      <div className="mx-auto w-1/2 ">
+        <form className="card-body">
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Name</span>
+            </label>
+            <input
+              type="text"
+              placeholder="name"
+              className="input input-bordered"
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Photo URL</span>
+            </label>
+            <input
+              type="text"
+              placeholder="Photo URL"
+              className="input input-bordered"
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input
+              type="email"
+              placeholder="email"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Password</span>
+            </label>
+            <input
+              type="password"
+              placeholder="password"
+              className="input input-bordered"
+              required
+            />
+            <label className="label">
+              <a href="#" className="label-text-alt link link-hover">
+                Forgot password?
+              </a>
+            </label>
+          </div>
+          <div className="form-control mt-6">
+            <button className="btn btn-primary">Register</button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
